4748b7ddcfe662176518f8b76875b42bfc99d522
Volodymyr Serdiukov <hhkphhkp15@gmail.com>
2022-08-16 21:46:05 +0300

    bigger image

--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -3,15 +3,15 @@
     <img
       alt="Quasar logo"
       src="~assets/quasar-logo-vertical.svg"
-      style="width: 200px; height: 200px"
-    >
+      style="width: 400px; height: 600px"
+    />
   </q-page>
 </template>
 
 <script>
-import { defineComponent } from 'vue'
+import { defineComponent } from "vue";
 
 export default defineComponent({
-  name: 'IndexPage'
-})
+  name: "IndexPage",
+});
 </script>
